ad4515a1fada0099bee6838ae20b26e402bd417d
Adam Horvath-Smith <horvath-smith.2@wright.edu>
2019-04-06 17:47:30 -0400

    Extract features from index

--- feature-extract.py
+++ feature-extract.py
@@ -11,4 +11,25 @@ dataset. The following steps are used:
     4. Generates a libsvm file with TF-IDF of each term
 """
 
+from sys import argv
+from index import InvertedIndex, index_newsgroups
+
+# Get input args
+newsgroups_root_dir = argv[1]
+feat_def_path = argv[2]
+class_def_path = argv[3]
+training_data_path = argv[4]
+
+# Generate index
+#index_newsgroups(newsgroups_root_dir, "idx_save.pkl")
+ii = InvertedIndex()
+ii.load("idx_save.pkl")
+
+# Write out feature/term pairs to feat_def_path
+feature_id = 0
+with open(feat_def_path, 'w') as outf:
+    for item in ii.items:
+        outf.write(str(feature_id) + "," + str(item) + "\n")
+        feature_id += 1
+
 
