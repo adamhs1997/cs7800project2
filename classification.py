a3fbff5e00e8445fff71548ffe2a06b90c30b26f
Adam Horvath-Smith <horvath-smith.2@wright.edu>
2019-04-11 18:26:32 -0400

    Complete base classifier comparison work

---
+++ classification.py
@@ -0,0 +1,58 @@
+# Adam Horvath-Smith
+# CS 7800 Project 2
+# Keke Chen
+
+from sklearn.datasets import load_svmlight_file
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+
+# Import the libSVM data file
+# --Testing only done on the TF-IDF data
+feature_vectors, targets = load_svmlight_file(r"training_data.txt")
+
+# Evaluate multinomial NBC
+print("--Multinomial NBC--")
+clf = MultinomialNB()
+scores = cross_val_score(clf, feature_vectors, targets, cv=5, scoring='f1_macro')
+print("F1 Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(clf, feature_vectors, targets, cv=5, scoring='precision_macro')
+print("Precision Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(clf, feature_vectors, targets, cv=5, scoring='recall_macro')
+print("Recall Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+print()
+
+# Evaluate Bernoulli NBC
+print("--Bernoulli NBC--")
+clf = BernoulliNB()
+scores = cross_val_score(clf, feature_vectors, targets, cv=5, scoring='f1_macro')
+print("F1 Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(clf, feature_vectors, targets, cv=5, scoring='precision_macro')
+print("Precision Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(clf, feature_vectors, targets, cv=5, scoring='recall_macro')
+print("Recall Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+print()
+
+# Evaluate KNN
+print("--KNN--")
+clf = KNeighborsClassifier()
+scores = cross_val_score(clf, feature_vectors, targets, cv=5, scoring='f1_macro')
+print("F1 Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(clf, feature_vectors, targets, cv=5, scoring='precision_macro')
+print("Precision Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(clf, feature_vectors, targets, cv=5, scoring='recall_macro')
+print("Recall Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+print()
+
+# Evaluate SVM
+print("--SVM--")
+clf = SVC()
+scores = cross_val_score(clf, feature_vectors, targets, cv=5, scoring='f1_macro')
+print("F1 Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(clf, feature_vectors, targets, cv=5, scoring='precision_macro')
+print("Precision Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(clf, feature_vectors, targets, cv=5, scoring='recall_macro')
+print("Recall Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+print()
